9a82569e2f20dcdf5b572e5d5871ebca879200ef
LMBSP1025 <100066810+LMBSP1025@users.noreply.github.com>
2023-03-04 14:31:54 +0900

    d

--- js/title.js
+++ js/title.js
@@ -1,82 +1,45 @@
-import { GlowParticle } from "./glowparticle.js";
+this.canvas = document.getElementById("titlecanvas");
+this.canvas.style.zIndex = 1;
+this.stageWidth = window.innerWidth;
+this.stageHeight = window.innerHeight;
+this.ctx = this.canvas.getContext('2d');
 
-const COLORS = [
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
-    { r: 249, g: 221, b: 182 },
+this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
 
 
-];
-class App {
-    constructor() {
-        this.canvas = document.getElementById("titlecanvas");
-        this.canvas.style.zIndex = 1;
-        this.stageWidth = window.innerWidth;
-        this.stageHeight = window.innerHeight;
-        this.ctx = this.canvas.getContext('2d');
+this.totalParticles = 50;
+this.particles = [];
+this.maxRadius = 50;
+this.minRadius = 40;
 
-        this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1;
+window.addEventListener('resize', this.resize.bind(this), false);
+this.resize();
 
+window.requestAnimationFrame(this.animate.bind(this));
 
-        this.totalParticles = 50;
-        this.particles = [];
-        this.maxRadius = 50;
-        this.minRadius = 40;
-
-        window.addEventListener('resize', this.resize.bind(this), false);
-        this.resize();
-
-        window.requestAnimationFrame(this.animate.bind(this));
-    }
-
-    resize() {
-        this.stageWidth = window.innerWidth;
-        this.stageHeight = window.innerHeight;
+function resize() {
+    this.stageWidth = window.innerWidth;
+    this.stageHeight = window.innerHeight;
 
 
-        this.canvas.width = this.stageWidth * this.pixelRatio;
-        this.canvas.height = this.stageHeight * this.pixelRatio;
-        this.ctx.scale(this.pixelRatio, this.pixelRatio);
-        this.ctx.globalCompositeOperation = 'darker';
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+    this.ctx.globalCompositeOperation = 'darker';
 
-        this.createParticles();
-    }
-    createParticles() {
-        let curColor = 0;
-        this.particles = [];
+}
 
-        for (let i = 0; i < this.totalParticles; i++) {
-            const item = new GlowParticle(
-                Math.random() * this.stageWidth,
-                Math.random() * this.stageHeight,
-                (this.maxRadius - this.minRadius) * this.minRadius,
-                COLORS[curColor]
-            );
-            if (++curColor >= COLORS.length) {
-                curColor = 0;
-            }
-            this.particles[i] = item;
-        }
-    }
-    animate() {
+function animate() {
 
-        this.ctx.fillStyle = "white";
-        window.requestAnimationFrame(this.animate.bind(this));
-        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    this.ctx.fillStyle = "#f9ddb6";
+    window.requestAnimationFrame(this.animate.bind(this));
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
 
-        for (let i = 0; i < this.totalParticles; i++) {
-            const item = this.particles[i];
-            item.animate(this.ctx, this.stageWidth, this.stageHeight);
-        }
+    for (let i = 0; i < this.totalParticles; i++) {
+        const item = this.particles[i];
+        item.animate(this.ctx, this.stageWidth, this.stageHeight);
     }
 }
 window.onload = () => {
-    new App();
+
 }
